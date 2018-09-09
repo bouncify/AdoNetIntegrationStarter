14cfbd49905d928852fc0be1f094f5f856331a49
James Rounce <james.rounce@outlook.com>
2018-09-09 13:15:46 -0700

    starter

---
+++ AdoNetIntegration/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdoNetIntegration.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    /// <summary>  
+    /// Home controller class.   
+    /// </summary>  
+    [Authorize]
+    public class HomeController : Controller
+    {
+        #region Index method.  
+        /// <summary>  
+        /// Index method.   
+        /// </summary>  
+        /// <returns>Returns - Index view</returns>  
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+        #endregion
+    }
+}
---
+++ AdoNetIntegration/Models/AccountViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace AdoNetIntegration.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username
+        {
+            get;
+            set;
+        }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password
+        {
+            get;
+            set;
+        }
+    }
+}
